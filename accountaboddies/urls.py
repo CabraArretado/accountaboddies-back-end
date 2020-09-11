3c9720d3df3670aeb4dc8acfbebfbc1336868096
felipetempus <felipemouradev@gmail.com>
2020-09-11 11:08:26 -0400

    error

--- accountaboddies/urls.py
+++ accountaboddies/urls.py
@@ -13,14 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.authtoken import views
+from accountaboddiesapi.views import login_user
 
 router = routers.DefaultRouter(trailing_slash=False)
+# Routes go here
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('login/', login_user),
     path('api-token-auth/', obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
